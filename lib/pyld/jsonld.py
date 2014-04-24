e5d871383bc8fc3bf56d74e6c53c1b9c702050b7
Yoan Blanc <yoan@dosimple.ch>
2014-04-24 11:38:02 +0200

    version 26.0.5.0-dev with python 2.6 changes.
    Putting 26 as a version prefix prevents pip to grab any official ones as
    long as version is not reached.

--- lib/pyld/jsonld.py
+++ lib/pyld/jsonld.py
@@ -14,7 +14,7 @@ JSON-LD.
 
 __copyright__ = 'Copyright (c) 2011-2013 Digital Bazaar, Inc.'
 __license__ = 'New BSD license'
-__version__ = '0.5.0'
+__version__ = '26.0.5.0-dev'
 
 __all__ = [
     'compact', 'expand', 'flatten', 'frame', 'from_rdf', 'to_rdf',
